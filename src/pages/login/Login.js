f74d74c45bf3cafed8b0ceba05e1a1508110cac1
Nelson <habunel123@outlook.com>
2022-03-23 23:48:45 +0200

    ewallet login

---
+++ src/pages/login/Login.js
@@ -0,0 +1,219 @@
+import React, { useState } from "react";
+import {
+  Grid,
+  CircularProgress,
+  Typography,
+  Button,
+  Tabs,
+  Tab,
+  TextField,
+  Fade,
+} from "@material-ui/core";
+import { withRouter } from "react-router-dom";
+// styles
+import useStyles from "./styles";
+// context
+import { useUserDispatch, loginUser } from "../../context/UserContext";
+
+function Login(props) {
+  var classes = useStyles();
+
+  // global
+  var userDispatch = useUserDispatch();
+
+  // local
+  var [isLoading, setIsLoading] = useState(false);
+  var [error, setError] = useState(null);
+  var [activeTabId, setActiveTabId] = useState(0);
+  var [nameValue, setNameValue] = useState("");
+  var [loginValue, setLoginValue] = useState("");
+  var [passwordValue, setPasswordValue] = useState("");
+
+  return (
+    <Grid container className={classes.container}>
+      <div className={classes.logotypeContainer}>
+        <img width="150px" height="160px" src="https://th.bing.com/th/id/R.8c25e69872f021e133526ad102f26023?rik=qgLpAwrQ%2fOrriA&pid=ImgRaw&r=0" alt="logo" className={classes.logotypeImage} />
+        <Typography className={classes.logotypeText}>Welcome to ewallet</Typography>
+      </div>
+      <div className={classes.formContainer}>
+        <div className={classes.form}>
+          <Tabs
+            value={activeTabId}
+            onChange={(e, id) => setActiveTabId(id)}
+            indicatorColor="primary"
+            textColor="primary"
+            centered
+          >
+            <Tab label="Login" classes={{ root: classes.tab }} />
+            <Tab label="New User" classes={{ root: classes.tab }} />
+          </Tabs>
+          {activeTabId === 0 && (
+            <React.Fragment>             
+              <Fade in={error}>
+                <Typography color="secondary" className={classes.errorMessage}>
+                  Something is wrong with your login or password :(
+                </Typography>
+              </Fade>
+              <TextField
+                id="email"
+                InputProps={{
+                  classes: {
+                    underline: classes.textFieldUnderline,
+                    input: classes.textField,
+                  },
+                }}
+                value={loginValue}
+                onChange={e => setLoginValue(e.target.value)}
+                margin="normal"
+                placeholder="Email Adress"
+                type="email"
+                fullWidth
+              />
+              <TextField
+                id="password"
+                InputProps={{
+                  classes: {
+                    underline: classes.textFieldUnderline,
+                    input: classes.textField,
+                  },
+                }}
+                value={passwordValue}
+                onChange={e => setPasswordValue(e.target.value)}
+                margin="normal"
+                placeholder="Password"
+                type="password"
+                fullWidth
+              />
+              <div className={classes.formButtons}>
+                {isLoading ? (
+                  <CircularProgress size={26} className={classes.loginLoader} />
+                ) : (
+                  <Button
+                    disabled={
+                      loginValue.length === 0 || passwordValue.length === 0
+                    }
+                    onClick={() =>
+                      loginUser(
+                        userDispatch,
+                        loginValue,
+                        passwordValue,
+                        props.history,
+                        setIsLoading,
+                        setError,
+                      )
+                    }
+                    variant="contained"
+                    color="primary"
+                    size="large"
+                  >
+                    Login
+                  </Button>
+                )}
+                <Button
+                  color="primary"
+                  size="large"
+                  className={classes.forgetButton}
+                >
+                  Forget Password
+                </Button>
+              </div>
+            </React.Fragment>
+          )}
+          {activeTabId === 1 && (
+            <React.Fragment>             
+              <Typography variant="h2" className={classes.subGreeting}>
+                Create your account
+              </Typography>
+              <Fade in={error}>
+                <Typography color="secondary" className={classes.errorMessage}>
+                  Something is wrong with your login or password :(
+                </Typography>
+              </Fade>
+              <TextField
+                id="name"
+                InputProps={{
+                  classes: {
+                    underline: classes.textFieldUnderline,
+                    input: classes.textField,
+                  },
+                }}
+                value={nameValue}
+                onChange={e => setNameValue(e.target.value)}
+                margin="normal"
+                placeholder="Full Name"
+                type="email"
+                fullWidth
+              />
+              <TextField
+                id="email"
+                InputProps={{
+                  classes: {
+                    underline: classes.textFieldUnderline,
+                    input: classes.textField,
+                  },
+                }}
+                value={loginValue}
+                onChange={e => setLoginValue(e.target.value)}
+                margin="normal"
+                placeholder="Email Adress"
+                type="email"
+                fullWidth
+              />
+              <TextField
+                id="password"
+                InputProps={{
+                  classes: {
+                    underline: classes.textFieldUnderline,
+                    input: classes.textField,
+                  },
+                }}
+                value={passwordValue}
+                onChange={e => setPasswordValue(e.target.value)}
+                margin="normal"
+                placeholder="Password"
+                type="password"
+                fullWidth
+              />
+              <div className={classes.creatingButtonContainer}>
+                {isLoading ? (
+                  <CircularProgress size={26} />
+                ) : (
+                  <Button
+                    onClick={() =>
+                      loginUser(
+                        userDispatch,
+                        loginValue,
+                        passwordValue,
+                        props.history,
+                        setIsLoading,
+                        setError,
+                      )
+                    }
+                    disabled={
+                      loginValue.length === 0 ||
+                      passwordValue.length === 0 ||
+                      nameValue.length === 0
+                    }
+                    size="large"
+                    variant="contained"
+                    color="primary"
+                    fullWidth
+                    className={classes.createAccountButton}
+                  >
+                    Create your account
+                  </Button>
+                )}
+              </div>           
+              
+            </React.Fragment>
+          )}
+        </div>
+        <Typography color="primary" className={classes.copyright}>
+          © 2022 Ewallet, Solvit. All rights reserved.
+        </Typography>
+      </div>
+    </Grid>
+  );
+}
+
+export default withRouter(Login);
